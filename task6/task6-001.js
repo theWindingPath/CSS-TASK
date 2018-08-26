ce69d6f76d9e0c1fb97d7f30550f9f97d064028d
theWindingPath <2958202145@qq.com>
2018-08-27 02:16:18 +0800

    使用移动端布局完成任务6

---
+++ task6/task6-001.js
@@ -0,0 +1,21 @@
+/*这一部分是自适应使用rem
+  当屏幕大于640时，Html为font-size:100px 6.4rem=640px
+  */
+(function (doc,win) {
+    var docEl =doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' :'resize',
+    recalc = function() {
+        var clientWidth = docEl.clientWidth;
+        if (!clientWidth) return;
+        if (clientWidth >= 720  /*640*/) {
+            docEl.style.fontSize = '100px';
+        } else {
+            /*当屏幕小于640px时 屏幕自适应为当前屏幕大小*/
+            docEl.style.fontSize = 100 * (clientWidth / 720 /* 640 */) + 'px';
+        }
+    };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt,recalc,false);
+    doc.addEventListener('DOMContentLoaded',recalc,false);
+    recalc();
+})(document,window);
